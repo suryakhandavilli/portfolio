3d9bdb9c63511101232c509b30ae1972cbc7f22f
Surya Khandavilli <Khandavillisurya@gmail.com>
2020-03-03 16:39:26 +0530

    Updates

---
+++ precache-manifest.db78f7088cd4ad61eace0d77342b7d6a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7485834555192ce4c2fac0bd7da9abd8",
+    "url": "/suryakhandavilli/portfolio.git/index.html"
+  },
+  {
+    "revision": "fe96a970c337aadefc6e",
+    "url": "/suryakhandavilli/portfolio.git/static/css/main.c40e6b93.chunk.css"
+  },
+  {
+    "revision": "a19af913698cb70a6ee1",
+    "url": "/suryakhandavilli/portfolio.git/static/js/2.edcebfa1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/suryakhandavilli/portfolio.git/static/js/2.edcebfa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe96a970c337aadefc6e",
+    "url": "/suryakhandavilli/portfolio.git/static/js/main.e2c9ccec.chunk.js"
+  },
+  {
+    "revision": "f0ef66441088dad72173",
+    "url": "/suryakhandavilli/portfolio.git/static/js/runtime-main.8a0e5633.js"
+  },
+  {
+    "revision": "5f732fc63f45a9bad86b35e19f25967e",
+    "url": "/suryakhandavilli/portfolio.git/static/media/Billgenerator.5f732fc6.png"
+  },
+  {
+    "revision": "8f8e97a061cdfc5512f449cf85fab6a7",
+    "url": "/suryakhandavilli/portfolio.git/static/media/GoogleTimer.8f8e97a0.png"
+  },
+  {
+    "revision": "9920b2f97e3923821bc052b42f8bb81c",
+    "url": "/suryakhandavilli/portfolio.git/static/media/Marksrecorder.9920b2f9.png"
+  },
+  {
+    "revision": "cbb3bf7d1da0442e05877f5e3e2811f9",
+    "url": "/suryakhandavilli/portfolio.git/static/media/OMDB.cbb3bf7d.png"
+  },
+  {
+    "revision": "d861c973fc635bf604a2f41139a300cd",
+    "url": "/suryakhandavilli/portfolio.git/static/media/Todo.d861c973.png"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/suryakhandavilli/portfolio.git/static/media/bootstrapimg.71e42b5a.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/suryakhandavilli/portfolio.git/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/suryakhandavilli/portfolio.git/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/suryakhandavilli/portfolio.git/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "8ec9bacf8bc78b8ad161b5e9a2a16e87",
+    "url": "/suryakhandavilli/portfolio.git/static/media/jquery.8ec9bacf.svg"
+  },
+  {
+    "revision": "ba99aecd075dddfb01b64b37724165fb",
+    "url": "/suryakhandavilli/portfolio.git/static/media/material-ui.ba99aecd.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/suryakhandavilli/portfolio.git/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "e36d17c992f39c9be81d9b03e8651665",
+    "url": "/suryakhandavilli/portfolio.git/static/media/web_developer.e36d17c9.png"
+  }
+]);
